c3aa1dcbbf8ad8ad337b698f9f2da0609da2d6d8
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2024-09-29 23:29:16 +0300

    Add main code

--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -3,8 +3,15 @@
 #include "src/hex.h"
 
 int main(int argc, char **argv) {
-    auto h1 = ::hex::Hex("CD");
-    const auto h2 = ::hex::Hex("AB");
-    h1 -= h2;
-    std::cout << h1.get_value();
+    std::string v1, v2;
+
+    std::cout << "Enter first number" << std::endl;
+    std::cin >> v1;
+    std::cout << "Enter second number" << std::endl;
+    std::cin >> v2;
+
+    auto h1 = ::hex::Hex(v1);
+    const auto h2 = ::hex::Hex(v2);
+
+    std::cout << (h1 + h2).get_value();
 }
